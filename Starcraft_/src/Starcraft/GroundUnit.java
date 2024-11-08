4ec9f06bac54fc682807968a2ff6d690d1853000
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 15:08:56 +0900

    Create GroundUnit Tank, Marin, SCV

--- Starcraft_/src/Starcraft/GroundUnit.java
+++ Starcraft_/src/Starcraft/GroundUnit.java
@@ -8,4 +8,27 @@ abstract class GroundUnit extends Unit{
 	
 }
 
+class Tank extends GroundUnit{
+
+	Tank() {
+		super("TANK", 200, 100);
+	}
+	
+}
+
+class Marin extends GroundUnit{
+
+	Marin() {
+		super("MARIN", 100, 80);
+	}
+
+}
+
+class SCV extends GroundUnit{
+
+	SCV() {
+		super("SCV", 50, 50);
+	}
+
+}
 
