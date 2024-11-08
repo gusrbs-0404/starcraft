e6fd2354550e7b2170c4f9490a66cbea40171df4
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 14:29:18 +0900

    Create StarcraftGame class 생성 및 싱글톤 패턴

---
+++ Starcraft_/src/Starcraft/StarcraftGame.java
@@ -0,0 +1,18 @@
+package Starcraft;
+
+public class StarcraftGame {
+
+	private void StarcaftGame() {
+
+	}
+
+	private static StarcraftGame instance = new StarcraftGame();
+
+	public static StarcraftGame getInstance() {
+		return instance;
+	}
+
+	public static void run() {
+
+	}
+}
