9accc5bcc373bc4cc795190c645876968fbb4af9
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 15:51:24 +0900

    Add class SCV - void repair() moehod 추가

--- Starcraft_/src/Starcraft/GroundUnit.java
+++ Starcraft_/src/Starcraft/GroundUnit.java
@@ -1,22 +1,22 @@
 package Starcraft;
 
-abstract class GroundUnit extends Unit{
+abstract class GroundUnit extends Unit {
 
 	GroundUnit(String name, int hp, int mp) {
 		super(name, hp, mp);
 	}
-	
+
 }
 
-class Tank extends GroundUnit implements Repairable{
+class Tank extends GroundUnit implements Repairable {
 
 	Tank() {
 		super("TANK", 200, 100);
 	}
-	
+
 }
 
-class Marin extends GroundUnit implements Repairable{
+class Marin extends GroundUnit implements Repairable {
 
 	Marin() {
 		super("MARIN", 100, 80);
@@ -24,11 +24,25 @@ class Marin extends GroundUnit implements Repairable{
 
 }
 
-class SCV extends GroundUnit implements Repairable{
+class SCV extends GroundUnit implements Repairable {
 
 	SCV() {
 		super("SCV", 50, 50);
 	}
 
-}
+	void repair(Readable unit) {
+		if (unit instanceof Unit) {
+			Unit target = (Unit) unit;
+			while (target.hp < target.MAX_HP) {
+				System.out.println(target + "|| 회복중");
+				target.hp++;
+				try {
+					Thread.sleep(300);
+				} catch (Exception e) {
+				}
+			}
+			System.out.println("<<< 회복 완료 >>> ");
+		}
+	}
 
+}
