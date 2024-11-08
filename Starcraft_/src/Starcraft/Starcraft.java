4bd8b53e42568a9daa8fb7119e55a7ee5b72a5b9
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 15:54:15 +0900

    Refactor 정렬

--- Starcraft_/src/Starcraft/Starcraft.java
+++ Starcraft_/src/Starcraft/Starcraft.java
@@ -3,9 +3,8 @@ package Starcraft;
 public class Starcraft {
 
 	public static void main(String[] args) {
-		
-		StarcaftGame game = StarcraftGame.getInstance();
+
+		StarcraftGame game = StarcraftGame.getInstance();
 		game.run();
 	}
-
 }
