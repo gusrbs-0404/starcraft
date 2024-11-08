627380d5afdef9be20e4734882dbeeb7845da820
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 14:43:18 +0900

    Add 변수, 생성자, toStrong @Override

--- Starcraft_/src/Starcraft/Unit.java
+++ Starcraft_/src/Starcraft/Unit.java
@@ -1,5 +1,23 @@
 package Starcraft;
 
 abstract class Unit {
-
+	final int MAX_HP;
+	final int MAX_MP;
+	int hp;
+	int mp;
+	String name;
+	
+	Unit(String name, int hp, int mp){
+		MAX_HP = hp;
+		MAX_MP = mp;
+		this.hp = hp;
+		this.mp = mp;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		String massgae = String.format("[%s] | [HP : %d/%d | MP : %d/%d] ", name, hp, MAX_HP, mp, MAX_MP);
+		return 	massgae;
+	}
 }
