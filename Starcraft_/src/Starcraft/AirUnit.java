06eaeb4c6d13ffabd5abd3736e0beb525082ffe7
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 15:21:27 +0900

    Create AirUnit calss Carrier, Corsair 생성

--- Starcraft_/src/Starcraft/AirUnit.java
+++ Starcraft_/src/Starcraft/AirUnit.java
@@ -8,3 +8,29 @@ abstract class AirUnit extends Unit {
 
 	abstract void fluing();
 }
+
+class Carrier extends AirUnit {
+
+	Carrier() {
+		super("CARRIER", 250, 150);
+	}
+
+	@Override
+	void fluing() {
+		System.out.println("공중 유닛!");
+	}
+
+}
+
+class Corsair extends AirUnit {
+
+	Corsair() {
+		super("CORSAIR", 150, 100);
+	}
+
+	@Override
+	void fluing() {
+		System.out.println("공중 유닛!");
+	}
+
+}
