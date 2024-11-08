accf4316a59e696486eaf403c9013b293d03be34
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-08 15:59:42 +0900

    Add final int 1~5, printUint() method

--- Starcraft_/src/Starcraft/StarcraftGame.java
+++ Starcraft_/src/Starcraft/StarcraftGame.java
@@ -2,6 +2,12 @@ package Starcraft;
 
 public class StarcraftGame {
 
+	final int TANK = 1;
+	final int Marin = 2;
+	final int SCV = 3;
+	final int Carrier = 4;
+	final int Corsair = 5;
+
 	private void StarcaftGame() {
 
 	}
@@ -14,8 +20,11 @@ public class StarcraftGame {
 
 	public static void run() {
 		newUnit();
+		while (true) {
+			printUint();
+		}
 	}
-	
+
 	private static void newUnit() {
 		Tank tank = new Tank();
 		Marin marine = new Marin();
@@ -23,4 +32,13 @@ public class StarcraftGame {
 		Carrier carrier = new Carrier();
 		Corsair corsair = new Corsair();
 	}
+
+	private static void printUint() {
+		System.out.println("1)탱크");
+		System.out.println("2)마린");
+		System.out.println("3)SCV");
+		System.out.println("4)케리어");
+		System.out.println("5)커세어");
+	}
+
 }
